4cf6aaf6a0457a8aff32be91dc13e68f633e4cf5
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2022-10-21 00:31:13 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto maiores earum dolor illo laudantium ipsa perferendis eos quam cumque, eligendi doloremque sequi quisquam voluptatibus ea magnam odit, porro, explicabo veniam cupiditate. Molestias dolore sapiente autem harum? Deleniti quas quia, laborum dignissimos odio unde ut molestias consequatur, magnam quaerat eveniet id. Sequi voluptates ratione quasi repellat animi blanditiis voluptatibus sunt dicta dolorem, odit modi, corporis ducimus eius adipisci explicabo est eum nihil dolorum. Repellendus voluptatibus, hic quibusdam excepturi reiciendis sequi eveniet laudantium dolorem, saepe voluptatum accusantium consequuntur expedita quisquam eum qui! Architecto fuga ea quidem aspernatur exercitationem autem impedit veritatis doloribus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto maiores earum dolor illo laudantium ipsa perferendis eos quam cumque, eligendi doloremque sequi quisquam voluptatibus ea magnam odit, porro, explicabo veniam cupiditate. Molestias dolore sapiente autem harum? Deleniti quas quia, laborum dignissimos odio unde ut molestias consequatur, magnam quaerat eveniet id. Sequi voluptates ratione quasi repellat animi blanditiis voluptatibus sunt dicta dolorem, odit modi, corporis ducimus eius adipisci explicabo est eum nihil dolorum. Repellendus voluptatibus, hic quibusdam excepturi reiciendis sequi eveniet laudantium dolorem, saepe voluptatum accusantium consequuntur expedita quisquam eum qui! Architecto fuga ea quidem aspernatur exercitationem autem impedit veritatis doloribus!
+    </p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
